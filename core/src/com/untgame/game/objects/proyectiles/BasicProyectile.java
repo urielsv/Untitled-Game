a8afcbd633da5f1cf0e8c0cb63829b10884a3832
CTepedino <ctepedino@itba.edu.ar>
2022-12-07 23:53:23 -0300

    BasicProyectile

---
+++ core/src/com/untgame/game/objects/proyectiles/BasicProyectile.java
@@ -0,0 +1,34 @@
+package com.untgame.game.objects.proyectiles;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Pool;
+
+public class BasicProyectile{
+    public static final int SPEED = 500;
+    public boolean remove;
+    private static Texture texture;
+    float x,y;
+
+    public BasicProyectile(float x, float y) {
+        this.x=x;
+        this.y=y;
+        if (texture == null){
+            texture = new Texture("badlogic.jpg");
+        }
+    }
+
+    public void update (float deltaTime){
+        y +=SPEED * deltaTime;
+        if (y>Gdx.graphics.getHeight())
+            remove = true;
+    }
+
+    public void render(SpriteBatch batch){
+        batch.draw(texture, x, y);
+
+    }
+}
+
